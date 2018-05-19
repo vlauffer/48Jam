89caa8d27e4b0a80a83f84943d25f4b60a62d6fc
vlauffer <vlauffer@gmail.com>
2018-05-19 01:09:09 -0400

    Not sure what im doing lol

---
+++ Assets/PriceUpdater.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PriceUpdater : MonoBehaviour {
+
+	public GameObject marker;
+	public Text priceDisplay;
+	// Use this for initialization
+	void Start () {
+		priceDisplay = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		priceDisplay.text = marker.transform.position.y.ToString();
+	}
+}
---
+++ Assets/RandomLerper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomLerper : MonoBehaviour {
+
+
+	public float range;
+	public float randomSpeed;
+
+
+	public Transform startPos;
+	public Transform endPos;
+	float startTime;
+	float d2D;
+	// Use this for initialization
+	
+	void Start(){
+		startTime=Time.time;
+		d2D = Vector2.Distance(startPos.position,endPos.position);
+	}
+	void Update(){
+		float currentDur=Time.time - startTime;
+		float fraction = currentDur / d2D;
+		transform.position = Vector2.Lerp(startPos.position,endPos.position,fraction);
+
+		if(currentDur==d2D){
+			Debug.Log("done");
+		}
+	}
+
+	public void ChangeLocation(){
+		
+	}
+}
