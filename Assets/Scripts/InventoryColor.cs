0b446717f5a27980658daa83052496ba40ecba0f
gavinyahna <31702834+gavinyahna@users.noreply.github.com>
2018-05-19 23:24:12 +0200

    added shooting, building, and inventory

---
+++ Assets/Scripts/InventoryColor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryColor : MonoBehaviour {
+    private static int blueCount;
+    private static int redCount;
+    private static int goldCount;
+    public Text blueText;
+    public Text redText;
+    public Text goldText;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        blueText.text = blueCount.ToString();
+        redText.text = redCount.ToString();
+        goldText.text = goldCount.ToString();
+	}
+
+    public static void addCount(string color, int number)
+    {
+        if(color == "blue")
+        {
+            blueCount += number;
+        } else if(color == "red")
+        {
+            redCount += number;
+        } else
+        {
+            goldCount += number;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public GameObject redShot;
+    public GameObject blueShot;
+    public Transform shotSpawn;
+    public Transform cameraForward;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            fireRed();
+        } else if (Input.GetMouseButtonDown(1))
+        {
+            fireBlue();
+        }
+
+	}
+
+    void fireBlue()
+    {
+        var bullet = (GameObject)Instantiate(
+            blueShot,
+            shotSpawn.position,
+            shotSpawn.rotation);
+    }
+
+    void fireRed()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            redShot,
+            shotSpawn.position,
+            shotSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().AddForce(cameraForward.forward * 1000);
+    }
+}
